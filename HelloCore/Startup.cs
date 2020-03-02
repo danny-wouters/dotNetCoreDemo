d6a0ae16638ab7c0a78191c551401ffdf51298a3
Danny Wouters <danny_wouters9@hotmail.com>
2020-03-02 17:10:40 +0100

    Add swagger to project so we can test our api calls

--- HelloCore/Startup.cs
+++ HelloCore/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace HelloCore
 {
@@ -45,6 +46,12 @@ namespace HelloCore
                 .AddEntityFrameworkStores<HelloCoreContext>();
 
             services.AddDbContext<HelloCoreContext>(options => options.UseSqlServer(Configuration.GetConnectionString("HelloCoreConnection")));
+
+            // Register the Swagger generator, defining 1 ore more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -73,7 +80,18 @@ namespace HelloCore
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
 
-            CreateUserRoles(services).Wait();
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                //c.RoutePrefix = string.Empty;
+            });
+
+            //CreateUserRoles(services).Wait();
         }
 
         private async Task CreateUserRoles(IServiceProvider serviceProvider)
